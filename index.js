39b07768603c3561fab721f0945ffcd8b48f1342
Taylor Watson <taylortwatson@gmail.com>
2019-03-08 22:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function shout(string) { return string.toUpperCase() }
-function logShout(string) { return
+function logShout(string) { return console.log
 
 function whisper(string) { return string.toLowerCase()}
 
